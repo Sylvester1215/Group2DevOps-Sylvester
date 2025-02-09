188b74309b151564d7fcc0f469547e7cecbd2c5a
Sylvester1215 <sylvesterpoh02@gmail.com>
2025-02-10 00:10:00 +0800

    Clean

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
-module.exports = {
-  apps: [
-    {
-      name: "app1",
-      script: "./index.mjs",
-      watch: true,  // Auto-restart on file changes
-      instances: "max",  // Run multiple instances (scales with CPU cores)
-      exec_mode: "cluster",  // Enable clustering for better performance
-      env: {
-        NODE_ENV: "production",
-        PORT: 5050,  // Ensure your app runs on the correct port
-      }
-    }
-  ]
-};
+// module.exports = {
+//   apps: [
+//     {
+//       name: "app1",
+//       script: "./index.mjs",
+//       watch: true,  // Auto-restart on file changes
+//       instances: "max",  // Run multiple instances (scales with CPU cores)
+//       exec_mode: "cluster",  // Enable clustering for better performance
+//       env: {
+//         NODE_ENV: "production",
+//         PORT: 5050,  // Ensure your app runs on the correct port
+//       }
+//     }
+//   ]
+// };
